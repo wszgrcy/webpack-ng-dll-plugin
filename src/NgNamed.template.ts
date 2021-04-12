927877b8f79d28d820d25eeb7c7cf7896bc62c55
wszgrcy <wszgrcy@gmail.com>
2021-04-12 20:22:04 +0800

    命名导入异常报错

--- src/NgNamed.template.ts
+++ src/NgNamed.template.ts
@@ -7,6 +7,9 @@
   (window as any).exportNgNamed = exportNgNamed;
    /** 导入ng命名 */
   function importNgNamed(key: string) {
+    if (!object[key]) {
+      throw new TypeError(`${key} is not exportNgNamed`)
+    }
     return object[key];
   }
   (window as any).importNgNamed = importNgNamed;
