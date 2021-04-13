742a28f6cf89d2ed476e5d85a97dfdd19f514318
wszgrcy <wszgrcy@gmail.com>
2021-04-13 22:51:09 +0800

    module-manifest

--- src/remote-startup/module-manifest/module-manifest.template.ts
+++ src/remote-startup/module-manifest/module-manifest.template.ts
@@ -1,27 +1,39 @@
 (() => {
+  interface AttrGroup {
+    nomodule?: string;
+    integrity?: string;
+    crossOrigin?: string;
+    defer?: string;
+    type?: string;
+    src?: string;
+    href?: string;
+  }
+  function loadScript(config: AttrGroup) {
+    let script = document.createElement('script');
+    script.charset = 'utf-8';
+    script.src = config.src;
+    script.defer = config.defer === '';
+    script.noModule = config.nomodule === '';
+    if (config.integrity) {
+      script.integrity = config.integrity;
+    }
+    if (config.crossOrigin !== 'none') {
+      script.crossOrigin = config.crossOrigin;
+    }
+    if (config.type) {
+      script.type = config.type;
+    }
+    document.head.appendChild(script);
+    return script;
+  }
   /** 载入远程模块,项目中使用 */
   function loadRemoteModuleManifest(config: {
-    main: string;
-    mainName: string;
-    scripts: any[];
-    stylesheets: any[];
+    mainScripts: (AttrGroup & { name: string })[];
+    scripts: AttrGroup[];
+    stylesheets: AttrGroup[];
   }): Promise<any> {
     config.scripts.forEach((item) => {
-      let script = document.createElement('script');
-      script.charset = 'utf-8';
-      script.src = item.src;
-      script.defer = item.defer;
-      script.noModule = item.nomodule === '';
-      if (item.integrity) {
-        script.integrity = item.integrity;
-      }
-      if (item.crossOrigin !== 'none') {
-        script.crossOrigin = item.crossOrigin;
-      }
-      if (item.type) {
-        script.type = item.type;
-      }
-      document.head.appendChild(script);
+      loadScript(item);
     });
     let styleLoading = Promise.all(
       config.stylesheets.map((item) => {
@@ -41,9 +53,13 @@
     );
 
     return styleLoading
-      .then((list) => {
-        return window.loadRemoteModule(config.main, config.mainName);
-      })
+      .then(() =>
+        Promise.race(
+          config.mainScripts.map((item) =>
+            (window as any).loadRemoteModule(loadScript(item), item.name)
+          )
+        )
+      )
       .catch((error) => {
         throw error;
       });
