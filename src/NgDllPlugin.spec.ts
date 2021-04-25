1067fb6e1b614a9b338fafabb4424e8e1b9f3793
chen-yang <wszgrcy@gmail.com>
2021-04-25 11:28:33 +0800

    test: NgDllPlugin

--- src/NgDllPlugin.spec.ts
+++ src/NgDllPlugin.spec.ts
@@ -1,39 +1,63 @@
 import { describeBuilder } from '@angular-devkit/build-angular/src/testing';
-import { buildWebpackBrowserGenerate } from '../test/test-builder/browser';
+import {
+  BROWSER_BUILDER_INFO,
+  buildWebpackBrowserGenerate,
+  DEFAULT_ANGULAR_CONFIG,
+} from '../test/test-builder/browser';
 import * as path from 'path';
+import { setNgDllPlugin } from './helper';
 
+let angularConfig = { ...DEFAULT_ANGULAR_CONFIG, vendorChunk: false };
 describeBuilder(
-  buildWebpackBrowserGenerate((config) => {
-    return config;
+  buildWebpackBrowserGenerate((options, context) => {
+    return (config) => {
+      options;
+      context;
+      setNgDllPlugin(
+        config,
+        {
+          output: {
+            filename: 'dll.js',
+          },
+          ngDllPluginOptions: {
+            path: path.resolve(context.workspaceRoot, 'dist', 'manifest.json'),
+            name: 'Dll',
+            format: true,
+            filter: {
+              mode: 'full',
+            },
+          },
+        },
+        options
+      );
+      return config;
+    };
   }),
-  {
-    name: 'test-builder:browser',
-    schemaPath: path.resolve(
-      process.cwd(),
-      'test',
-      'test-builder',
-      'browser',
-      'schema.json'
-    ),
-  },
+  BROWSER_BUILDER_INFO,
   (harness) => {
-    describe('NgDllPlugin', () => {
+    describe.only('NgDllPlugin', () => {
       it('可执行', async () => {
-        harness.useTarget('build', {
-          outputPath: 'dist',
-          index: 'src/index.html',
-          main: 'src/main.ts',
-          polyfills: 'src/polyfills.ts',
-          tsConfig: 'src/tsconfig.app.json',
-          progress: false,
-          assets: ['src/favicon.ico', 'src/assets'],
-          styles: ['src/styles.css'],
-          scripts: [],
-        });
+        harness.useTarget('build', angularConfig);
 
         let result = await harness.executeOnce();
         result;
-        expect(harness.hasFile('dist/runtime-es5.js')).toBe(true);
+
+        expect(harness.hasFile('dist/testProject/dll.js')).toBe(true);
+        let content = harness.readFile('dist/testProject/dll.js');
+        expect(content).toContain('var Dll =');
+        expect(harness.hasFile('dist/manifest.json'));
+
+        let manifest = harness.readFile('dist/manifest.json');
+        let manifestJson = JSON.parse(manifest);
+        expect(manifestJson.name).toBe('Dll');
+
+        expect(
+          Object.keys(manifestJson.content).some(
+            (name) =>
+              ['@angular/core', 'rxjs'].some((item) => name.includes(item)) &&
+              !name.includes('$$_lazy_route_resource')
+          )
+        ).toBe(true);
       });
     });
   }
--- test/test-builder/browser/index.ts
+++ test/test-builder/browser/index.ts
@@ -5,18 +5,38 @@ import {
   ExecutionTransformer,
 } from '@angular-devkit/build-angular';
 import * as webpack from 'webpack';
-
+import * as path from 'path';
 export type CustomWebpackBrowserSchema = BrowserBuilderOptions;
 
 export function buildWebpackBrowserGenerate(
-  webpackConfiguration: ExecutionTransformer<webpack.Configuration>
+  webpackConfiguration: (
+    options: BrowserBuilderOptions,
+    context: BuilderContext
+  ) => ExecutionTransformer<webpack.Configuration>
 ) {
   return (
     options: CustomWebpackBrowserSchema,
     context: BuilderContext
   ): ReturnType<typeof executeBrowserBuilder> => {
     return executeBrowserBuilder(options, context, {
-      webpackConfiguration,
+      webpackConfiguration: webpackConfiguration(options, context),
     });
   };
 }
+
+export const BROWSER_BUILDER_INFO = {
+  name: 'test-builder:browser',
+  schemaPath: path.resolve(__dirname, 'schema.json'),
+};
+
+export const DEFAULT_ANGULAR_CONFIG = {
+  outputPath: 'dist/testProject',
+  index: 'src/index.html',
+  main: 'src/main.ts',
+  polyfills: 'src/polyfills.ts',
+  tsConfig: 'src/tsconfig.app.json',
+  progress: false,
+  assets: ['src/favicon.ico', 'src/assets'],
+  styles: ['src/styles.css'],
+  scripts: [],
+};
