fc696e7bacb045af8d6d76426f44a1aa963c63d8
wszgrcy <wszgrcy@gmail.com>
2021-06-05 19:22:21 +0800

    refactor: 远程模块启动模板

--- src/remote/module/RemoteModuleStartupMainTemplatePlugin.ts
+++ src/remote/module/RemoteModuleStartupMainTemplatePlugin.ts
@@ -2,6 +2,7 @@ import * as webpack from 'webpack';
 import { SyncWaterfallHook } from 'tapable';
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import { RuntimeGlobals } from 'webpack';
 const { Template } = webpack;
 /** 远程模块启动
  * 插入一段脚本,用于加载`LoadRemoteModulePlugin`处理过的项目
@@ -11,6 +12,21 @@ export class RemoteModuleStartupMainTemplatePlugin {
     compiler.hooks.thisCompilation.tap(
       'RemoteModuleStartupMainTemplatePlugin',
       (compilation) => {
+        compilation.hooks.additionalChunkRuntimeRequirements.tap(
+          'RemoteModuleStartupMainTemplatePlugin',
+          (chunk, set) => {
+            console.log(chunk.name);
+
+            if (chunk.name === 'main') {
+              set.add(RuntimeGlobals.startup);
+              compilation.addRuntimeModule(
+                chunk,
+                new RemoteModuleStartupMainTemplateModule(),
+                compilation.chunkGraph
+              );
+            }
+          }
+        );
         (
           (compilation.mainTemplate.hooks as any)
             .bootstrap as SyncWaterfallHook<any>
@@ -25,5 +41,17 @@ export class RemoteModuleStartupMainTemplatePlugin {
         });
       }
     );
+  }
+}
+
+class RemoteModuleStartupMainTemplateModule extends webpack.RuntimeModule {
+  constructor() {
+    super('RemoteModuleStartupMainTemplateModule');
+  }
+  generate() {
+    return Template.asString([
+      '// RemoteModuleStartupMainTemplatePlugin',
+      readFileSync(path.resolve(__dirname, './module.template.js')).toString(),
+    ]);
   }
 }
