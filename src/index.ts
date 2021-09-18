b3a69bd05b12defda39ead583f68d74f7d83ecb2
wszgrcy <wszgrcy@gmail.com>
2021-09-18 15:44:39 +0800

    feat: 增加监听资源变更

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './NgDllPlugin';
 export * from './remote';
 export * from './ng-named';
 export * from './types';
+export * from './wait-asset-until/WaitAssetUntilPlugin';
---
+++ src/wait-asset-until/WaitAssetUntilPlugin.ts
@@ -0,0 +1,116 @@
+import * as webpack from 'webpack';
+export interface WaitAssetUtilFileOptions {
+  filePath: string;
+  isDir?: boolean;
+  listen?: boolean;
+}
+export class WaitAssetUtilPlugin {
+  constructor(
+    private options: {
+      files: WaitAssetUtilFileOptions[];
+      intervalTime?: number;
+      timeout?: number | undefined;
+    }
+  ) {
+    this.options.intervalTime =
+      typeof this.options.intervalTime === 'number'
+        ? this.options.intervalTime
+        : 5000;
+  }
+  apply(compiler: webpack.Compiler) {
+    compiler.hooks.thisCompilation.tap('WaitAssetUtilPlugin', (compilation) => {
+      this.options.files.forEach((item) => {
+        compilation.fileDependencies.add(item.filePath);
+      });
+      compilation.hooks.processAssets.tapAsync(
+        'WaitAssetUtilPlugin',
+        (compilationAssets, cb) => {
+          this.fileAllExist(this.options.files, compilation).then((result) => {
+            if (result.status) {
+              cb();
+            } else {
+              compilation.logger.warn('file not all exist!');
+              cb();
+            }
+          });
+        }
+      );
+    });
+  }
+
+  fileAllExist(
+    files: WaitAssetUtilFileOptions[],
+    compilation: webpack.Compilation
+  ) {
+    return this._fileAllExist(files, compilation, Date.now());
+  }
+  private async _fileAllExist(
+    files: WaitAssetUtilFileOptions[],
+    compilation: webpack.Compilation,
+    startTime: number
+  ): Promise<{ status: boolean }> {
+    let promiseList: WaitAssetUtilFileOptions[] = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      let promise = new Promise<
+        Parameters<
+          Parameters<webpack.Compilation['inputFileSystem']['stat']>[1]
+        >[1]
+      >((res, rej) => {
+        compilation.inputFileSystem.stat(file.filePath, (err, result) => {
+          if (result) {
+            res(result);
+          } else if (err) {
+            rej(err);
+          } else {
+            throw new Error('unknown');
+          }
+        });
+      })
+        .then((result) => {
+          if (file.isDir && result.isDirectory()) {
+          } else if (!file.isDir && result.isFile()) {
+          } else {
+            throw new Error('not match');
+          }
+        })
+        .catch(() => {
+          promiseList.push(file);
+        })
+        .then(() => {
+          compilation.inputFileSystem.purge(file.filePath);
+        });
+      await promise;
+    }
+    if (!promiseList.length) {
+      return {
+        status: true,
+      };
+    }
+    if (
+      !this.options.timeout ||
+      Date.now() - startTime < this.options.timeout
+    ) {
+      let res: (
+        value:
+          | {
+              status: boolean;
+            }
+          | PromiseLike<{
+              status: boolean;
+            }>
+      ) => void;
+      let resultPromise = new Promise<{ status: boolean }>((result) => {
+        res = result;
+      });
+      setTimeout(() => {
+        res(this._fileAllExist(promiseList, compilation, startTime));
+      }, this.options.intervalTime);
+      return resultPromise;
+    } else {
+      return {
+        status: false,
+      };
+    }
+  }
+}
